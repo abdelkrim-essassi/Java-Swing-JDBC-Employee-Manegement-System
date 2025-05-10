4be7a5729774c3de5b5e3b909ff221f1799cf629
Abdelkarim Sassi <84048872+abdelkrim-essassi@users.noreply.github.com>
2025-05-10 10:48:37 +0100

    Update SingletonConnection.java

--- DAO/SingletonConnection.java
+++ DAO/SingletonConnection.java
@@ -1,25 +1,34 @@
 package DAO;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class SingletonConnection {
-	private static Connection connection;
-	private SingletonConnection() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Employees?useSSL=false", "root", "");
-		} catch (ClassNotFoundException e) {
+    private static Connection connection;
 
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	
-	}
-	public static Connection getInstnce() {
-		if(connection == null) new SingletonConnection();
-	return connection;
-	}
+    private SingletonConnection() {}
 
+    public static Connection getInstance() {
+        if (connection == null) {
+            try {
+                Properties props = new Properties();
+                props.load(new FileInputStream("config.properties"));
+
+                String url = props.getProperty("db.url");
+                String user = props.getProperty("db.user");
+                String password = props.getProperty("db.password");
+
+                Class.forName("com.mysql.cj.jdbc.Driver"); 
+                connection = DriverManager.getConnection(url, user, password);
+
+            } catch (IOException | ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
 }
